036be8efb5b8280aa0b50e5b342192469f0208f7
iksdehh <120662278+iksdehh@users.noreply.github.com>
2023-06-04 18:08:39 +0200

    aktuelle Version

--- task6/NavMaze.java
+++ task6/NavMaze.java
@@ -2,8 +2,6 @@ package Java2.task6;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,23 +9,22 @@ import java.util.List;
 import java.util.Map;
 
 public class NavMaze extends JFrame  {
-
+    JFrame fenster = new JFrame("GrapicMaze");
     List<Point> getSolution = new ArrayList<>();
     private final char[][] maze;
     int counter = 1;
-    JFrame fenster = new JFrame("GrapicMaze");
     public static JPanel panel = new JPanel(new GridLayout(6, 6, 1, 1));
     public static JPanel panel2 = new JPanel(new GridLayout(6, 6, 1, 1));
     public  JPanel jpanel;
     public JButton zurueck_Button = new JButton( "zurück" );
     public  JButton vor_Button = new JButton("vor");
 
-    public NavMaze(String title, char[][] maze) {
+    public NavMaze(char[][] maze) {
+        this.maze = maze;
+        setFenster();
 
-        fenster.setSize(500, 500);
-        fenster.setLocationRelativeTo(null); // Um das Fenster in der Mitte des Bildschirms zu öffnen
-        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // beendet das Programm beim Schließen des Fensters
         panel.setBackground(Color.BLACK);
+
 
         JPanel overlayPanel = new JPanel();
         overlayPanel.setLayout(new OverlayLayout(overlayPanel));
@@ -36,71 +33,53 @@ public class NavMaze extends JFrame  {
         overlayPanel.add(panel2);
 
 
-        //Einstellungen der Buttons
-        vor_Button.setSize(40,20);
-        // vor_Button.addActionListener(e -> System.out.println("ich bin der vor button"));
-        zurueck_Button.setSize(40,20);
-        // zurueck_Button.addActionListener(e -> System.out.println("ich bin der zurück button"));
+        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER,10,10));    //JPanel für BorderLayout für den southBereich - FlowChart wird hinzugefügt
+
+        setVor_Button();
+        setZurueck_Button();
 
 
-        vor_Button = new JButton("Vor");
-        vor_Button.addActionListener(new ActionListener() {
+        fenster.add(overlayPanel, BorderLayout.CENTER);//das GridLayout wird im Center platziert
+        fenster.add(southPanel, BorderLayout.SOUTH);//das southPanel wird im South-Bereich platziert
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (counter > 11){
-                    counter = 1;
-                }
-                plusAktion(counter);
-                counter++;
-            }
-        });
-        zurueck_Button = new JButton("Zurück");
-        zurueck_Button.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (counter==1){
-                    plusAktion(counter);
-                    return;
-                }
-                counter--;
-                plusAktion(counter);
-            }
-        });
-
-        //JPanel für BorderLayout für den southBereich - FlowChart wird hinzugefügt
-        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER,10,10));
-
-        //das GridLayout wird im Center platziert und das southPanel im South-Bereich
-        fenster.add(overlayPanel, BorderLayout.CENTER);
-        fenster.add(southPanel, BorderLayout.SOUTH);
         southPanel.add(zurueck_Button);
         southPanel.add(vor_Button);
 
         fenster.setVisible(true);
-        this.maze = maze;
     }
 
-    public static void main(String[] args) {
-
-        char[][] maze =
-                        {{' ','X',' ','X',' ',' '},
-                        {' ','X',' ',' ',' ','X'},
-                        {' ',' ','X','X',' ','X'},
-                        {'X',' ',' ',' ',' ','X'},
-                        {' ',' ',' ','X',' ','X'},
-                        {'X','X',' ',' ',' ',' '}};
-
-        NavMaze mymaze = new NavMaze("hallo", maze);
-
-        mymaze.canExit(0,0);
-
-        mymaze.printMaze();
-
-        mymaze.getSolution.forEach(System.out::println);
-
+    private void setFenster(){
+        fenster.setSize(500, 500);
+        fenster.setLocationRelativeTo(null); // Um das Fenster in der Mitte des Bildschirms zu öffnen
+        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // beendet das Programm beim Schließen des Fensters
     }
+
+    private void setVor_Button(){
+
+        vor_Button.setSize(40,20);
+        zurueck_Button.setSize(40,20);
+
+        vor_Button = new JButton("Vor");
+        vor_Button.addActionListener(e -> {
+            if (counter > 11){
+                counter = 1;
+            }
+            plusAktion(counter);
+            counter++;
+        });
+    }
+    private void setZurueck_Button(){
+        zurueck_Button = new JButton("Zurück");
+        zurueck_Button.addActionListener(e -> {
+            if (counter==1){
+                plusAktion(counter);
+                return;
+            }
+            counter--;
+            plusAktion(counter);
+        });
+    }
+
     public boolean canExit(int i, int j) {
 
         int n = maze.length;
@@ -117,11 +96,10 @@ public class NavMaze extends JFrame  {
                 || canExit(i+1,j) /* unten */|| canExit(i,j+1) /* rechts */
                 || canExit(i-1,j) /* oben */ || canExit(i,j-1) /* links */
         ) {
-            //Weißes Feld mit Kreis
-            maze[i][j] = '+';
+
+            maze[i][j] = '+';   //Weißes Feld mit Kreis
             return true;
         }
-
         return false;
     }
 
@@ -149,7 +127,6 @@ public class NavMaze extends JFrame  {
     private void plusAktion(int counter) {
 
         panel2.removeAll();
-        int x= 0;
 
         Map<Integer, Integer> hashMap = new HashMap<>();
         hashMap.put(1, 0);
@@ -164,17 +141,13 @@ public class NavMaze extends JFrame  {
         hashMap.put(10, 34);
         hashMap.put(11, 35);
 
-        if(hashMap.containsKey(counter)) {
-            x = hashMap.get(counter);
-        }
-
+        int x = hashMap.getOrDefault(counter, -1);
 
         for (int i = 0; i < 36; i++) {
             JPanel jpanel = new JPanel();
             if(i == x){
                 jpanel.setLayout(new BorderLayout());
                 jpanel.add(new KreisPanel(), BorderLayout.CENTER);
-                panel2.add(jpanel);
             } else {
                 jpanel.setOpaque(false);
             }
@@ -182,8 +155,21 @@ public class NavMaze extends JFrame  {
         }
         fenster.setVisible(true);
     }
+    public static void main(String[] args) {
+        char[][] maze =
+                {{' ','X',' ','X',' ',' '},
+                        {' ','X',' ',' ',' ','X'},
+                        {' ',' ','X','X',' ','X'},
+                        {'X',' ',' ',' ',' ','X'},
+                        {' ',' ',' ','X',' ','X'},
+                        {'X','X',' ',' ',' ',' '}};
 
-    class KreisPanel extends JPanel {
+        NavMaze mymaze = new NavMaze(maze);
+        mymaze.canExit(0,0);
+        mymaze.printMaze();
+        mymaze.getSolution.forEach(System.out::println);
+    }
+    static class KreisPanel extends JPanel {
         @Override
         protected void paintComponent(Graphics g){
             super.paintComponent(g);
