f7699f1c801bfca883f604ceee40cced36d86d87
iksdehh <120662278+iksdehh@users.noreply.github.com>
2023-06-16 20:02:29 +0200

    Aufgabe 10 fertig

---
+++ KlausurVorbereitung/Aufgabe10.java
@@ -0,0 +1,18 @@
+package Java2.KlausurVorbereitung;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Implementieren Sie eine Methode zum Entfernen aller Mehrfach-Einträge aus der Liste mit folgender Signatur: public void delCopies(List l).
+// Nach Aufruf der Methode enthält die als Parameter übergebene Liste nur unterschiedliche, nicht gleiche Objekte. Die Reihenfolge darf nicht verändert werden.
+public class Aufgabe10 {
+    public static void main(String[] args) {
+        List list = Arrays.asList("Hallo", "Hallo", "wie", "gehts?");
+        delCopies(list);
+    }
+    public static void delCopies(List<String> l){
+        l.stream().distinct().collect(Collectors.toList()).forEach(System.out::println);
+    }
+}
