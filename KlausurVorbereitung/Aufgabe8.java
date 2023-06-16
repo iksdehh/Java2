5ef85f9e9b3545965738908e334d02fb5ca64597
iksdehh <120662278+iksdehh@users.noreply.github.com>
2023-06-16 19:46:53 +0200

    Aufgabe 8 fertig

---
+++ KlausurVorbereitung/Aufgabe8.java
@@ -0,0 +1,34 @@
+package Java2.KlausurVorbereitung;
+
+import Java2.task4.TextFileFormatException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+//loadmethode aus der Übung!
+public class Aufgabe8 {
+
+    static List<String> liste = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        liste = load("C:\\Users\\ReneW\\IdeaProjects\\Practise\\src\\Java2\\KlausurVorbereitung\\Aufgabe8Test");
+        System.out.println(liste);
+    }
+    public static List<String> load (String filename) throws IOException{
+        List<String> geladen = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))){
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                if (!line.trim().isEmpty()) {
+                    //ich weiß nicht ob dies so besprochen ist oder soll wir die Module tatsächlich speichern?
+                    geladen.add(line);
+                }
+            }
+        } catch (IOException | ArrayIndexOutOfBoundsException exception) {
+            throw new TextFileFormatException("Liste fehlerhaft!");
+        }
+        return geladen;
+    }
+}
